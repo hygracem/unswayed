03ca16be2b77f1b49923b83aca06449c8283e4f3
Grace Jang <hygracem@gmail.com>
2020-05-10 23:53:38 +0900

    controller wip

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,13 +1,23 @@
 class ArticlesController < ApplicationController
   def index
+    @articles = Article.all
+    # where(date: today.beginning_of_day..today.end_of_day)
   end
 
   def show
+    @article = Article.find(params[:id])
   end
 
   def create
+  #   @article = Artist.new(article_params)
+  #   @article.save
+  #   redirect_to article_path(@article)
   end
 
   def destroy
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :country)
   end
 end
