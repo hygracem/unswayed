fd8987b6982f67e5c3f0408e49645d1b2d3a3aac
Grace Jang <hygracem@gmail.com>
2020-06-28 02:42:05 +0900

    added news api for articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,22 @@
+require 'open-uri'
+require 'rest-client'
 class ArticlesController < ApplicationController
-# require 'news-api'
+  skip_before_action :authenticate_user!
+
+  def fetch_keyword_articles(keyword)
+    header = {
+      Authorization: "Bearer #{ENV['NEWS_API_KEY']}"
+    }
+    articles_response = RestClient.get("http://newsapi.org/v2/everything?q=#{keyword}", header)
+    JSON.parse(articles_response)["articles"].first["title"]
+  end
 
   def index
-    # @articles = Article.all("c48e5c6d51fa49b88c7598db0356abf7").to_h
-    @articles = Article.all
+    # @articles = policy_scope(Article.all, policy_scope_class: ArticlesPolicy::Scope)
+    # authorize @articles
+    @articles = policy_scope(fetch_keyword_articles('coronavirus'), policy_scope_class: ArticlesPolicy::Scope)
+    # had to force the policy
+    # need to save it
     # where(date: today.beginning_of_day..today.end_of_day)
   end
 
--- app/policies/articles_policy.rb
+++ app/policies/articles_policy.rb
@@ -1,7 +1,12 @@
 class ArticlesPolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
-      scope.all
+      # scope.all -> why undefined method all error?
+      scope
     end
+  end
+
+  def index?
+    true
   end
 end
