52e4da21f47f5391cfe678ee5755670614a1c9ac
Grace Jang <hygracem@gmail.com>
2020-07-29 00:36:37 +0900

    homepage edited wip

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,11 @@
+require 'open-uri'
+require 'rest-client'
+
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
   def home
+    # @articles = policy_scope(fetch_keyword_articles('coronavirus').select { |x| x["publishedAt"].partition("T").first.between?((Date.today - 60).to_s, Date.today.to_s) }, policy_scope_class: ArticlesPolicy::Scope)
+    @articles = Article.all
   end
 
   def dashboard
